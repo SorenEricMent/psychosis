2a18311edc58de944e8c3696ebf362ed29b6731a
Yunchuan "Winslow" Hu <winslows@students.cs.ubc.ca>
2024-03-14 11:28:44 -0700

    add titlebar, todo: fix reflection in Main

--- src/main/ui/MainContainer.java
+++ src/main/ui/MainContainer.java
@@ -1,13 +1,39 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainContainer {
     private JList list1;
     private JPanel mainContainer;
+    private JButton fileBtn;
+    private JToolBar globalToolbar;
+    private JButton helpBtn;
 
     public static void main() {
-        JFrame mainContainer = new JFrame("Psychosys Studio " + Main.getVersion());
+        System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Psychosis Studio");
+        Toolkit defToolkit = Toolkit.getDefaultToolkit();
+        java.lang.reflect.Field awtAppClassNameField = null;
+
+        try {
+            awtAppClassNameField = defToolkit.getClass().getDeclaredField("awtAppClassName");
+            awtAppClassNameField.setAccessible(true);
+            awtAppClassNameField.set(defToolkit, "Psychosis Studio");
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                 | UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
+        JFrame mainContainer = new JFrame("Psychosis Studio " + Main.getVersion());
+        ImageIcon img = new ImageIcon("./data/resources/logo.jpg");
+
+        mainContainer.setIconImage(img.getImage());
+
         mainContainer.setContentPane(new MainContainer().mainContainer);
         mainContainer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainContainer.pack();
